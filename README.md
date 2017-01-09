b4117aab97f03a1a9dff5397405850a918cdcd74
hady elsahar <hadyelsahar@gmail.com>
2017-01-09 13:48:24 +0100

    download of wikidata dumps
    _ wikidata label dumps
    - wikidata simple statements
    - wikidata same as links with dbpedia

--- README.md
+++ README.md
@@ -13,6 +13,21 @@ cd dbpedia-spotlight
 java -Xmx6g -jar dbpedia-spotlight-latest.jar en http://localhost:2222/rest 
 ```
 
+### Wikdata dumps
+
+Wikdata provides a [tool for exporting RDF dumps](http://tools.wmflabs.org/wikidata-exports/rdf/index.html)
+ 
+Simple RDF dumps were used in which each statement is represented in a triple and statements with qualifiers are omitted
+[Wikidata RDF dumps 20160801](http://tools.wmflabs.org/wikidata-exports/rdf/exports/20160801/dump_download.html)
+
+Sameas links between Wikidata and DBpedia are already extracted and can be found on [wikidata.dbpedia.org](http://wikidata.dbpedia.org/)
+
+The latest available version in this project is [Available on the extraction page from 20150330](http://wikidata.dbpedia.org/downloads/20150330/)
+the downloaded dump is available from here [20150330-sameas-all-wikis.ttl.bz2](http://wikidata.dbpedia.org/downloads/20150330/wikidatawiki-20150330-sameas-all-wikis.ttl.bz2)
+
+
+
+
 ### Feautres  
 
 | feature name                 | description                                                                      |
--- startup.sh
+++ startup.sh
@@ -2,9 +2,10 @@
 # this is a bash script to bootstart the project including downloading of datasets - setup of additional tools.
 
 # DBpedia spotlight Installation
-
+echo "downloading dbpedia spotlight ..."
 wget http://spotlight.sztaki.hu/downloads/dbpedia-spotlight-latest.jar
 wget http://spotlight.sztaki.hu/downloads/latest_models/en.tar.gz
+
 tar xzf en.tar.gz
 rm en.tar.gz
 mkdir dbpedia-spotlight
@@ -14,7 +15,30 @@ cd dbpedia-spotlight
 # screen -S dbpedia-spotlight -dm java -Xmx6g -jar dbpedia-spotlight-latest.jar en http://localhost:2222/rest  # uncomment for running dbpedia spotlight server
 cd ..
 
+echo "installing python client for dbpedia spotlight..."
 pip install pyspotlight
+
+# Downloading Wikidata
+echo "downloading wikidata dumps..."
+mkdir wikidata ; cd wikidata
+
+echo "download wikdata labels dump: wikidata-terms.nt.gz"
+wget http://tools.wmflabs.org/wikidata-exports/rdf/exports/20160801/wikidata-terms.nt.gz
+echo "download wikdata simple statements dump:  wikidata-simple-statements.nt.gz "
+wget http://tools.wmflabs.org/wikidata-exports/rdf/exports/20160801/wikidata-simple-statements.nt.gz
+echo "download DBpedia-Wikidata Same As links dump: wikidatawiki-20150330-sameas-all-wikis.ttl.bz2"
+wget http://wikidata.dbpedia.org/downloads/20150330/wikidatawiki-20150330-sameas-all-wikis.ttl.bz2
+echo "download instance of dump wikidata-instances.nt.gz"
+wget http://tools.wmflabs.org/wikidata-exports/rdf/exports/20160801/wikidata-instances.nt.gz
+echo "download class heirarchy dump: wikidata-taxonomy.nt.gz"
+wget http://tools.wmflabs.org/wikidata-exports/rdf/exports/20160801/wikidata-taxonomy.nt.gz
+
+echo "creating a csv dictionary out of wikidata english labels"
+zcat wikidata-terms.nt.gz | grep "@en" | cut -d" " -f1,3- | sed -e 's/ /\t/' | sed -E 's/[<>\"]//g'| sed -E 's/@.+//g' | sort | uniq > wikidata-terms-dict.csv
+
+
+
+
 
 
 
