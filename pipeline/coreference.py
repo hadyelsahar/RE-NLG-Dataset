fcd4283f0f58639b0c9b3a47fd6e9c22fae64107
hadyelsahar <hadyelsahar@gmail.com>
2017-03-20 17:44:41 +0100

    sample coreference file

---
+++ pipeline/coreference.py
@@ -0,0 +1,19 @@
+
+from pipeline import *
+
+class SimpleCoreference(BasePipeline):
+    """
+    a class for simple coreference
+    to replace all pronouns with the base class uri
+    """
+
+    def run(self, document):
+        """
+        :param document: Class Document. Document containing
+        :return:
+        """
+        # todo: fill in by pavlos
+        # get base class URI   document.uri
+
+
+
