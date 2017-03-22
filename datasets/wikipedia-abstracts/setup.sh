5809281fdfaca973c21e2d39ccebcda2097396b2
hadyelsahar <hadyelsahar@gmail.com>
2017-03-22 14:23:00 +0100

    script to download wikipedia-abstracts and convert them to csv file.

--- datasets/wikipedia-abstracts/setup.sh
+++ datasets/wikipedia-abstracts/setup.sh
@@ -1,2 +1,13 @@
 #!/usr/bin/env bash
+echo "Downloading dbpedia abstracts"
 wget http://downloads.dbpedia.org/2016-04/core-i18n/en/long_abstracts_en.ttl.bz2
+echo "unzipping .."
+bzip2 -dk long_abstracts_en.ttl.bz2  #unzip keep original
+echo "changing ttl to csv.."
+python prep_wiki_abstracts.py -i long_abstracts_en.ttl  -o dbpedia-abstracts.csv
+rm long_abstracts_en.ttl
+echo "Done!"
+
+
+
+
