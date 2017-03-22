cbc56c45c6c6eee108cdd142d57bb31d119d1d43
hadyelsahar <hadyelsahar@gmail.com>
2017-03-22 18:02:24 +0100

    updated running example

---
+++ run_example.py
@@ -0,0 +1,19 @@
+
+from pipeline.pipeline import *
+from pipeline.entitylinker import *
+from pipeline.triplealigner import *
+from pipeline.datareader import DBpediaAbstractsDataReader
+
+
+# Reading the DBpedia Abstracts Dataset
+reader = DBpediaAbstractsDataReader('./datasets/wikipedia-abstracts/csv/dbpedia-abstracts.csv', db_wd_mapping='./datasets/wikidata/dbpedia-wikidata-sameas-dict.csv')
+
+# Loading the WikidataSpotlightEntityLinker ... DBpedia Spotlight with mapping DBpedia URIs to Wikidata
+link = WikidataSpotlightEntityLinker('./datasets/wikidata/dbpedia-wikidata-sameas-dict.csv', support=1, confidence=0.2)
+align = NoSubjectAlign('./datasets/wikidata/sample-wikidata-triples.csv')
+
+for d in reader.read_documents():
+    d = link.run(d)
+    d = align.run(d)
+
+    print "Document Title: %s \t Number of Annotated Entities %s" % (d.title, len(d.triples))
