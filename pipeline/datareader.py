c86441c74cf4d727d2feaff101718b071bb80211
hadyelsahar <hadyelsahar@gmail.com>
2017-03-22 17:33:14 +0100

    Added a DataReader Class for DBpedia Abstracts Dataset

---
+++ pipeline/datareader.py
@@ -0,0 +1,56 @@
+from pipeline import *
+import csv
+
+class DBpediaAbstractsDataReader:
+    """
+    class with a default read_documents functions that yields Document iterator
+    """
+    def __init__(self, dataset_file, db_wd_mapping=None):
+        """
+        :param dataset_file: path of the dataset file
+        """
+
+        self.dataset_file = dataset_file
+
+        if db_wd_mapping is not None:
+            self.mappings = {}
+            with open(db_wd_mapping) as f:
+                print "loading DBpedia to Wikidata URI mappings..."
+
+                for l in f.readlines():
+                    tmp = l.split("\t")
+                    self.mappings[tmp[0].strip()] = tmp[1].strip()
+
+        else:
+            self.mappings = None
+
+    def read_documents(self):
+        """
+        function that yields iterator of documents, the URI of each document is the DBpedia URI
+        """
+        with open(self.dataset_file) as f:
+            read = csv.reader(f)
+
+            for l in read:
+                # extraction of title from DBpedia URI
+                title = l[0].replace("http://dbpedia.org/resource/", "").replace("_", " ")
+
+                if self.mappings is not None:
+                    if l[0] in self.mappings:
+                        l[0] = self.mappings[0]
+                    else:
+                        continue
+
+                document = Document(
+                    pageuri=l[0],
+                    title=title,
+                    text=l[1]
+                )
+
+                yield document
+
+
+
+
+
+
