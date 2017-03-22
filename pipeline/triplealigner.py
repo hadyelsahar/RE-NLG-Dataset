068c2d142c4ca64695849c220ae451d7548f85a4
hadyelsahar <hadyelsahar@gmail.com>
2017-03-22 14:24:45 +0100

    change name triples to wikidatatriples

--- pipeline/triplealigner.py
+++ pipeline/triplealigner.py
@@ -10,14 +10,14 @@ class NoSubjectAlign(BasePipeline):
     def __init__(self, triples_file):
         self.annotator_name = "NoSubject-Triple-aligner"
         # load triples in memory
-        self.triples = pd.read_csv(triples_file, sep="\t", names=["subject", "predicate", "object"])
+        self.wikidata_triples = pd.read_csv(triples_file, sep="\t", names=["subject", "predicate", "object"])
 
     def run(self, document):
         """
         :param: input document to align it's sentences with triples
         :return:
         """
-
+        document.triples = []
         for sid, (start, end) in enumerate(document.sentences_boundaries):
 
             # Getting sentence subject
@@ -25,18 +25,19 @@ class NoSubjectAlign(BasePipeline):
 
             # if subject already tagged use it if not use only the URI
             # entities in sentence
-            es = [j for j in document.entities if j.boundaires[0] >= start and j.boundaires[1] <= end]
+            es = [j for j in document.entities if j.boundaries[0] >= start and j.boundaries[1] <= end]
             e_sub = [j for j in es if j.uri == document.uri]
-            if e_sub > 0:
+            if len(e_sub) > 0:
                 subject = e_sub[0]
             else:
                 subject = Entity(document.uri, boundaries=None, surfaceform=document.title, annotator=self.annotator_name)
 
             for o in es:
-
-                predicates = self.triples[self.triples.subject == subject.uri][self.triples.object == o.uri].predicate.values
+                predicates = self.wikidata_triples[self.wikidata_triples.subject == subject.uri][self.wikidata_triples.object == o.uri].predicate.values
 
                 for pred in predicates:
+
+                    pred = Entity(pred, boundaries=None, surfaceform=None, annotator=self.annotator_name)
 
                     triple = Triple(subject=subject,
                                     predicate=pred,
