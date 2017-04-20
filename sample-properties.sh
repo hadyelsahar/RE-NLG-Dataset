42a8a5ff624087afd9e060dcecf1ee54647aa1bb
hadyelsahar <hadyelsahar@gmail.com>
2017-04-20 16:41:56 +0200

    download properties aliases

--- sample-properties.sh
+++ sample-properties.sh
@@ -3,5 +3,5 @@
 cd datasets/wikidata
 wget http://tools.wmflabs.org/wikidata-exports/rdf/exports/20160801/wikidata-properties.nt.gz
 
-zcat wikidata-properties.nt | grep "<http://www.w3.org/2000/01/rdf-schema#label>" | grep "@en " | sed 's/.\{5\}$//' | sed 's/> </>\t</g' | sed 's/> "/>\t"/g' > wikidata-properties.csv
+zcat wikidata-properties.nt | grep "@en " | sed 's/.\{5\}$//' | sed 's/> </>\t</g' | sed 's/> "/>\t"/g' > wikidata-properties.csv
 rm wikidata-properties.nt.gz
