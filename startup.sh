23a877d42fb1e3e76b42d0f3e287cb9896dcea77
hadyelsahar <hadyelsahar@gmail.com>
2017-04-21 17:41:16 +0200

    update startup.sh  [wikidata truthy]
    download wikidata truthy dumps
    remove downloading unused dumps

--- startup.sh
+++ startup.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 # this is a bash script to bootstart the project including downloading of datasets - setup of additional tools.
 
-# DBpedia spotlight Installation
+#################################
+# DBpedia spotlight Installation#
+#################################
 echo "downloading dbpedia spotlight ..."
 
 mkdir resources
 cd resources
 wget http://spotlight.sztaki.hu/downloads/dbpedia-spotlight-latest.jar
 wget http://spotlight.sztaki.hu/downloads/latest_models/en.tar.gz
-
-tar xzf en.tar.gz
-rm en.tar.gz
+tar xzf en.tar.gz ; rm en.tar.gz
 mkdir dbpedia-spotlight
 mv en dbpedia-spotlight
 mv dbpedia-spotlight-latest.jar dbpedia-spotlight
@@ -21,6 +21,9 @@ cd ..
 echo "installing python client for dbpedia spotlight..."
 pip install pyspotlight
 
+######################
+# SUtime Installation#
+######################
 echo "downloading SU time"
 pip install sutime
 mkdir sutime
@@ -30,46 +33,34 @@ unzip jars.zip?dl=0
 rm jars.zip?dl=0
 cd ../..
 
-
-# Downloading Wikidata
-cd datasets
+################################
+# Downloading Wikidata Triples #
+################################
 echo "downloading wikidata dumps..."
-mkdir wikidata ; cd wikidata
+cd datasets
+mkdir wikidata
+cd wikidata
 
-echo "download wikdata labels dump: wikidata-terms.nt.gz"
-wget http://tools.wmflabs.org/wikidata-exports/rdf/exports/20160801/wikidata-terms.nt.gz
-echo "download wikdata simple statements dump:  wikidata-simple-statements.nt.gz "
-wget http://tools.wmflabs.org/wikidata-exports/rdf/exports/20160801/wikidata-simple-statements.nt.gz
+# triples
+echo "download wikidata facts triples statements from wikidata truthy dump .."
+wget https://dumps.wikimedia.org/wikidatawiki/entities/20170418/wikidata-20170418-truthy-BETA.nt.bz2
+echo "make csv file out of nt .."
+## skipping labels and meta information and keep only wikidata props
+bzcat  wikidata-20170418-truthy-BETA.nt.bz2 | grep "/prop/direct/P" | sed -E 's/[<>"]//g'| sed -E 's/@.+//g' | cut -d" " -f1-3 | sed -E 's/\s/\t/g' > wikidata-triples.csv
 
-echo "make csv file out of nt"
-zcat  wikidata-simple-statements.nt.gz | sed -E 's/[<>\"]//g'| sed -E 's/@.+//g' | cut -d" " -f1-3 | sed -E 's/\s/\t/g' > wikidata-triples.csv
-
+# DBpedia -Wikidata Sameas
 echo "download DBpedia-Wikidata Same As links dump: wikidatawiki-20150330-sameas-all-wikis.ttl.bz2"
 wget http://wikidata.dbpedia.org/downloads/20150330/wikidatawiki-20150330-sameas-all-wikis.ttl.bz2
-echo "download instance of dump wikidata-instances.nt.gz"
-wget http://tools.wmflabs.org/wikidata-exports/rdf/exports/20160801/wikidata-instances.nt.gz
-echo "download class heirarchy dump: wikidata-taxonomy.nt.gz"
-wget http://tools.wmflabs.org/wikidata-exports/rdf/exports/20160801/wikidata-taxonomy.nt.gz
-
-echo "creating a csv dictionary out of wikidata english labels"
-zcat wikidata-terms.nt.gz | grep "@en" | cut -d" " -f1,3- | sed -e 's/ /\t/' | sed -E 's/[<>\"]//g'| sed -E 's/@.+//g' | sort | uniq > wikidata-labels-dict.csv
-
 echo "creating a csv dictionary out of english dbpedia uris to wikidata same as links"
 bzcat wikidatawiki-20150330-sameas-all-wikis.ttl.bz2 | grep "http://dbpedia.org" | awk '{ print $3 " " $1}'  | sed -e 's/wikidata\.dbpedia\.org\/resource/www.wikidata\.org\/entity/'  | sed -e 's/ /\t/' | sed -E 's/[<>\"]//g' > dbpedia-wikidata-sameas-dict.csv
 
+# Wikidata properties labels
+echo "download wikidata labels"
+wget http://tools.wmflabs.org/wikidata-exports/rdf/exports/20160801/wikidata-properties.nt.gz
+zcat wikidata-properties.nt | grep "http://www.w3.org/2000/01/rdf-schema#label\|http://www.w3.org/2004/02/skos/core#altLabel" | grep "@en " | sed 's/.\{5\}$//' | sed 's/> </>\t</g' | sed 's/> "/>\t"/g' > wikidata-properties.csv
+rm wikidata-properties.nt.gz
+
 cd ../..
-
-# Downloading DBpedia:
-mkdir datasets/dbpedia
-cd datasets/dbpedia
-echo "download dbpedia triples"
-wget http://downloads.dbpedia.org/2016-04/core-i18n/en/infobox_properties_en.ttl.bz2
-
-echo "download dbpedia ontology triples"
-wget http://downloads.dbpedia.org/2016-04/core-i18n/en/instance_types_en.ttl.bz2
-
-echo "make list of dbpedia types"
-bzcat instance_types_en.ttl.bz2 | cut -d" " -f3  | grep "<http://dbpedia.org/ontology/" | sed -E 's/[<>]//g'| sort | uniq > dbpedia-classes.txt
 
 
 
