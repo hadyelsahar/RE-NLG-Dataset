7aef90e136e3dc2be88e3bbe1daafcdb86d2fcfd
hadyelsahar <hadyelsahar@gmail.com>
2017-04-24 16:45:28 +0200

    adding start_doc variable to skip processed docs

--- run.py
+++ run.py
@@ -6,13 +6,15 @@ from pipeline.writer import JsonWriter
 from pipeline.coreference import *
 from utils.triplereader import *
 
+
+start_doc = 0   #start reading from document number #
+
 # Reading the DBpedia Abstracts Dataset
-reader = DBpediaAbstractsDataReader('./datasets/wikipedia-abstracts/csv/dbpedia-abstracts.csv', db_wd_mapping='./datasets/wikidata/dbpedia-wikidata-sameas-dict.csv')
+reader = DBpediaAbstractsDataReader('./datasets/wikipedia-abstracts/csv/dbpedia-abstracts.csv', db_wd_mapping='./datasets/wikidata/dbpedia-wikidata-sameas-dict.csv', skip=start_doc)
 
 # Loading the WikidataSpotlightEntityLinker ... DBpedia Spotlight with mapping DBpedia URIs to Wikidata
-#link = WikidataSpotlightEntityLinker('./datasets/wikidata/dbpedia-wikidata-sameas-dict.csv', support=10, confidence=0.4)
-#link = DBSpotlightEntityLinker(spotlight_url='http://model.dbpedia-spotlight.org/en/annotate')
-link = WikidataSpotlightEntityLinker(db_wd_mapping='./datasets/wikidata/dbpedia-wikidata-sameas-dict.csv')
+link = WikidataSpotlightEntityLinker('./datasets/wikidata/dbpedia-wikidata-sameas-dict.csv', support=10, confidence=0.4)
+
 coref = SimpleCoreference()
 trip_read = TripleReader('./datasets/wikidata/wikidata-triples.csv')
 Salign = SimpleAligner(trip_read)
@@ -20,7 +22,7 @@ Salign = SimpleAligner(trip_read)
 date = DateLinker()
 # SPOalign = SPOAligner(trip_read)
 NSalign = NoSubjectAlign(trip_read)
-writer = JsonWriter('./out', "re-nlg")
+writer = JsonWriter('./out', "re-nlg", startfile=start_doc)
 
 for d in reader.read_documents():
 
