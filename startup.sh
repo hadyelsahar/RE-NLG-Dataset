e9cace36b4cf7eb616cbd84d483e72252bde09eb
hadyelsahar <hadyelsahar@gmail.com>
2017-04-24 16:50:32 +0200

    adding download of dbpedia abstracts to startup.sh

--- startup.sh
+++ startup.sh
@@ -62,6 +62,23 @@ rm wikidata-properties.nt.gz
 
 cd ../..
 
+echo "Downloading dbpedia abstracts"
+cd ./datasets/wikipedia-abstracts/csv/
+wget http://downloads.dbpedia.org/2016-04/core-i18n/en/long_abstracts_en.ttl.bz2
+echo "unzipping .."
+bzip2 -dk long_abstracts_en.ttl.bz2  #unzip keep original
+echo "changing ttl to csv.."
+python prep_wiki_abstracts.py -i long_abstracts_en.ttl  -o dbpedia-abstracts.csv
+rm long_abstracts_en.ttl
+cd ../../../
+echo "Done!"
+
+
+
+
+
+
+
 
 
 
