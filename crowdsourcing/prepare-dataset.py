346ddf6da1dbd9ea4ef52ae53ad43f44984a3da8
hadyelsahar <hadyelsahar@gmail.com>
2017-05-02 17:58:36 +0200

    adding  global variables and save only 20

--- crowdsourcing/prepare-dataset.py
+++ crowdsourcing/prepare-dataset.py
@@ -24,7 +24,9 @@ args = parser.parse_args()
 
 # [text, triple1 , ... triple 10, annotator]
 data = []
-
+__MAX_TRIPLES__ = 10
+__MIN_TRIPLES__ = 2
+__SAVE_N__ = 20
 
 path_to_properties = os.path.join(os.path.dirname(__file__), '../datasets/wikidata/wikidata-properties.csv')
 properties = {}
@@ -44,7 +46,7 @@ for file in os.listdir(args.input):
              t = [x for x in d['triples'] if annotator_NosubSPO in x['annotator']]
              text = ""
 
-             if len(t) > 2:
+             if len(t) > __MIN_TRIPLES__:
 
                  row = []
                  maxsent = 0
@@ -57,17 +59,19 @@ for file in os.listdir(args.input):
 
 
                      propname =  properties[x['predicate']['uri']] if x['predicate']['uri'] in properties else x['predicate']['surfaceform']
+
+                     propname = "<b><font color=\"red\">" + propname + "</font></b>"
                      row.append("%s \t %s \t %s"% (d['title'], propname, x['object']['surfaceform']))
                      text = d['text'][0:d['sentences_boundaries'][maxsent][1]]
 
 
                  row = list(set(row))
 
-                 if len(row) > 10:
-                     row = row[0:10]
+                 if len(row) > __MAX_TRIPLES__:
+                     row = row[0:__MAX_TRIPLES__]
 
-                 elif len(row) < 10:
-                     row += [None] * (10 - len(row))
+                 elif len(row) < __MAX_TRIPLES__:
+                     row += [None] * (__MAX_TRIPLES__ - len(row))
 
                  row = [text] + row
                  row += [x['annotator']]
@@ -75,7 +79,7 @@ for file in os.listdir(args.input):
                  continue
 
              t = [x for x in d['triples'] if annotator_SPO in x['annotator']]
-             if len(t) > 2:
+             if len(t) > __MIN_TRIPLES__:
                  row = []
                  maxsent = 0
 
@@ -85,6 +89,7 @@ for file in os.listdir(args.input):
                          maxsent = x['sentence_id']
 
                      propname = properties[x['predicate']['uri']] if x['predicate']['uri'] in properties else x['predicate']['surfaceform']
+                     propname = "<b><font color=\"red\">" + propname + "</font></b>"
 
                      row.append(
                          "%s \t %s \t %s" % (d['title'], propname, x['object']['surfaceform']))
@@ -92,11 +97,11 @@ for file in os.listdir(args.input):
 
                  row = list(set(row))
 
-                 if len(row) > 10:
-                     row = row[0:10]
+                 if len(row) > __MAX_TRIPLES__:
+                     row = row[0:__MAX_TRIPLES__]
 
-                 elif len(row) < 10:
-                     row += [None] * (10 - len(row))
+                 elif len(row) < __MAX_TRIPLES__:
+                     row += [None] * (__MAX_TRIPLES__ - len(row))
 
                  row = [text] + row
                  row += [x['annotator']]
@@ -105,7 +110,7 @@ for file in os.listdir(args.input):
 
 
              t = [x for x in d['triples'] if annotator_nosub in x['annotator']]
-             if len(t) > 2:
+             if len(t) > __MIN_TRIPLES__:
                  row = []
                  maxsent = 0
 
@@ -122,25 +127,27 @@ for file in os.listdir(args.input):
 
                          continue
 
+                     propname = "<b><font color=\"red\">" + propname + "</font></b>"
                      row.append(
                          "%s \t %s \t %s" % (d['title'], propname, x['object']['surfaceform']))
                      text = d['text'][0:d['sentences_boundaries'][maxsent][1]]
 
                  row = list(set(row))
 
-                 if len(row) > 10:
-                     row = row[0:10]
+                 if len(row) > __MAX_TRIPLES__:
+                     row = row[0:__MAX_TRIPLES__]
 
-                 elif len(row) < 10:
-                     row += [None] * (10 - len(row))
+                 elif len(row) < __MAX_TRIPLES__:
+                     row += [None] * (__MAX_TRIPLES__ - len(row))
 
                  row = [text] + row
                  row += [x['annotator']]
                  data.append(row)
                  continue
+
 
              t = [x for x in d['triples'] if annotator_simple in x['annotator']]
-             if len(t) > 2:
+             if len(t) > __MIN_TRIPLES__:
                  row = []
                  maxsent = 0
 
@@ -157,35 +164,46 @@ for file in os.listdir(args.input):
                      else:
 
                          continue
-
+                     propname = "<b><font color=\"red\">" + propname + "</font></b>"
                      row.append(
                          "%s \t %s \t %s" % (d['title'], propname, x['object']['surfaceform']))
                      text = d['text'][0:d['sentences_boundaries'][maxsent][1]]
 
                  row = list(set(row))
 
-                 if len(row) > 10:
-                     row = row[0:10]
+                 if len(row) > __MAX_TRIPLES__:
+                     row = row[0:__MAX_TRIPLES__]
 
-                 elif len(row) < 10:
-                     row += [None] * (10 - len(row))
+                 elif len(row) < __MAX_TRIPLES__:
+                     row += [None] * (__MAX_TRIPLES__ - len(row))
 
                  row = [text] + row
                  row += [x['annotator']]
                  data.append(row)
                  continue
-
 
 
 names = ["Original Sentence"]
 
-for i in range(0, 10):
+for i in range(0, __MAX_TRIPLES__):
     names.append("Triple-Fact %s" % i)
 
-names += ["annotator-name"]
+names += ["annotator_name"]
 
 x = pd.DataFrame(data, columns=names)
-x.to_csv(args.out, encoding="utf-8")
+
+
+anns = set(x.annotator_name.values)
+filtereddata = []
+
+for ann in anns:
+    tmp = [l for l in data if l[-1] == ann]
+    filtereddata += tmp[0:__SAVE_N__]
+
+filteredx = pd.DataFrame(filtereddata, columns=names)
+filteredx.to_csv(args.out, encoding="utf-8")
+
+
 
 
 
