4fffda5ba18bbd40497b1b75f7024352caf0d146
Arslen <arslen.r@gmail.com>
2017-05-03 11:07:18 +0200

    Script 2 changed : now create a csv file

--- paper_scripts/script_2.py
+++ paper_scripts/script_2.py
@@ -17,25 +17,24 @@ args = parser.parse_args()
 
 stats = defaultdict(int)
 
-result = os.path.join(args.out, 'result_script_2.json')
+result = args.out
 path_to_json = args.input
 
 path_to_properties = os.path.join(os.path.dirname(__file__), '../datasets/wikidata/wikidata-properties.csv')
-properties = defaultdict(list)
+properties = defaultdict(str)
 
 with open(path_to_properties) as f:
     for l in csv.reader(f, delimiter='\t'):
         properties[l[0]] = l[2]
 
 json_files = [pos_json for pos_json in os.listdir(path_to_json) if pos_json.endswith('.json')]
-for js in json_files:
+for c, js in enumerate(json_files):
     with open(os.path.join(path_to_json, js)) as json_file:
-        print "Starting the file : " + str(os.path.join(path_to_json, js))
+        print "Starting the file : " + str(os.path.join(path_to_json, js)) + " .. %s / %s" % (c+1, len(json_files))
         for d in json.load(json_file):
             for t in d['triples']:
                 label = properties[t['predicate']['uri']]
                 stats["%s, %s, %s" % (t['annotator'], t['predicate']['uri'], label)] += 1
-    print "Finished the file : " + str(os.path.join(path_to_json, js))
 
 with open(result, 'w') as k:
     k.write(json.dumps(stats, indent=4))
