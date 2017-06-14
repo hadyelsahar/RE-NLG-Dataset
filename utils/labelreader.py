10d3ead058fb139225a21e7af476ae1003632f2f
Lucie <lucie.kaffee@gmail.com>
2017-06-14 19:38:45 +0200

    minor changes to start lang fallback

--- utils/labelreader.py
+++ utils/labelreader.py
@@ -1,10 +1,11 @@
 from collections import defaultdict
 
-# return a defaultdict {entity:{lang:{"label", "alias"}, lang:{"label", "alias", "alias"} }} 
+# create a defaultdict {entity:[label, alias, alias], entity:[label, alias]}
 # from a csv file of labels formatted as "Q12345 \t 'Count von Count' \t en"
+# TODO: language fallback
 class LabelReader:
 
-    def __init__(self, labels_file, lang):
+    def __init__(self, labels_file, lang=None):
 
         self.baseuri = "http://www.wikidata.org/entity/"
 
@@ -13,10 +14,20 @@ class LabelReader:
         with open(labels_file) as f:
             for l in f:
                 tmp = l.split("\t")
-                # TODO: language fallback
-                if tmp[2].strip() == lang:
-                    self.d[tmp[0].strip().replace(self.baseuri, "")].append(tmp[1].strip())
+                entity_id = tmp[0].strip().replace(self.baseuri, "")
 
+                #if no language is set
+                if lang == None:
+                    self.d[entity_id].append(tmp[1].strip())
+                    continue
+
+                #if language is set
+                if tmp[2].strip() == lang:
+                    self.d[entity_id].append(tmp[1].strip())
+                #start building something for language fallback
+                elif not self.d[entity_id]:
+                    self.fallback[entity_id].append({tmp[2].strip(): tmp[1].strip()})
+                    self.d[entity_id] = []
 
     def get(self, uri):
         p = self.d[uri.strip().replace(self.baseuri, "")]
