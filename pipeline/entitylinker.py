96a9c960c4bd5c21e071fa7c44e892a12a7faf58
Lucie <lucie.kaffee@gmail.com>
2017-06-20 15:11:51 +0200

    make the pipeline work

--- pipeline/entitylinker.py
+++ pipeline/entitylinker.py
@@ -229,33 +229,24 @@ class WikidataPropertyLinker(BasePipeline):
 
 class KeywordMatchingEntityLinker(BasePipeline):
 
-	def __init__(self):
+	def __init__(self, trip_read_items, label_read):
 		self.annotator_name = 'Keyword_Matching_Entity_Linker'
+		self.trip_read_items = trip_read_items
+		self.label_read = label_read
 
-	def run(self, document, labels):
-		'''
-		stop_words = ["i", "me", "my", "myself", "we", "our", "ours", "ourselves", "you", "your", "yours", "yourself",
-					  "yourselves", "he", "him", "his", "himself", "she", "her", "hers", "herself", "it", "its",
-					  "itself", "they", "them", "their", "theirs", "themselves", "what", "which", "who", "whom", "this",
-					  "that", "these", "those", "am", "is", "are", "was", "were", "be", "been", "being", "have", "has",
-					  "had", "having", "do", "does", "did", "doing", "a", "an", "the", "and", "but", "if", "or",
-					  "because", "as", "until", "while", "of", "at", "by", "for", "with", "about", "against", "between",
-					  "into", "through", "during", "before", "after", "above", "below", "to", "from", "up", "down",
-					  "in", "out", "on", "off", "over", "under", "again", "further", "then", "once", "here", "there",
-					  "when", "where", "why", "how", "all", "any", "both", "each", "few", "more", "most", "other",
-					  "some", "such", "no", "nor", "not", "only", "own", "same", "so", "than", "too", "very", "s", "t",
-					  "can", "will", "just", "don", "should", "now"]
-		'''
+	def run(self, document):
 
-		for label in labels:
-			for m in re.finditer(r"\b" + re.escape(label) + r"\b", document.text):
-				(start, end) = m.start(), m.end()
-				#if document.text[start:end] not in stop_words:
-				entity = Entity(document.docid,
-								boundaries=(start, end),
-								surfaceform=document.text[start:end],
-								annotator=self.annotator_name)
-				document.entities.append(entity)
+		for uri in self.trip_read_items.get(document.docid):
+			labels = self.label_read.get(uri)
+			for label in labels:
+				for m in re.finditer(r"\b" + re.escape(label) + r"\b", document.text):
+					(start, end) = m.start(), m.end()
+
+					entity = Entity(uri,
+									boundaries=(start, end),
+									surfaceform=document.text[start:end],
+									annotator=self.annotator_name)
+					document.entities.append(entity)
 
 		return document
 
--- run_example.py
+++ run_example.py
@@ -14,40 +14,36 @@ reader = DBpediaAbstractsDataReader('./datasets/wikipedia-abstracts/csv/sample-d
 # LabelReader for esperanto
 label_read = LabelReader('./datasets/wikidata/sample-wikidata-labels.csv', 'eo')
 trip_read_items = TripleReaderItems('./datasets/wikidata/sample-wikidata-triples.csv')
-ent_linker = KeywordMatchingEntityLinker()
+keyword_ent_linker = KeywordMatchingEntityLinker(trip_read_items, label_read)
 # Loading the WikidataSpotlightEntityLinker ... DBpedia Spotlight with mapping DBpedia URIs to Wikidata
 # link = WikidataSpotlightEntityLinker('./datasets/wikidata/dbpedia-wikidata-sameas-dict.csv', support=10, confidence=0.4)
 # link = DBSpotlightEntityLinker(spotlight_url='http://model.dbpedia-spotlight.org/en/annotate')
 #link = WikidataSpotlightEntityLinker(db_wd_mapping='./datasets/wikidata/sample-dbpedia-wikidata-sameas.csv', spotlight_url='http://model.dbpedia-spotlight.org/en/annotate')
 #coref = SimpleCoreference()
-#trip_read = TripleReader('./datasets/wikidata/sample-wikidata-triples.csv')
-#Salign = SimpleAligner(trip_read)
+trip_read = TripleReader('./datasets/wikidata/sample-wikidata-triples.csv')
+Salign = SimpleAligner(trip_read)
 #prop = WikidataPropertyLinker('./datasets/wikidata/wikidata-properties.csv')
 #date = DateLinker()
 #SPOalign = SPOAligner(trip_read)
-#NSalign = NoSubjectAlign(trip_read)
-#writer = JsonWriter('./out', "", 1)
+NSalign = NoSubjectAlign(trip_read)
+writer = JsonWriter('./out', "", 1)
 
 for d in reader.read_documents():
-    print d.title
-    for uri in trip_read_items.get(d.docid):
-        labels = label_read.get(uri)
-        if labels:
-            ent_linker.run(d, label_read.get(uri))
-    break
-    #print label_read.get(d.docid)
+	print d.title
+
+	#print label_read.get(d.docid)
 #    try:
-'''
-        d = link.run(d)
-        d = NSalign.run(d)
-        d = coref.run(d)
-        d = date.run(d)
-        d = Salign.run(d)
-        d = prop.run(d)
-        d = SPOalign.run(d)
-        writer.run(d)
-        print "Document Title: %s \t Number of Annotated Entities %s \t Number of Annotated Triples %s" % (d.title, len(d.entities), len(d.triples))
-'''
+	d = keyword_ent_linker.run(d)
+		#d = link.run(d)
+	d = NSalign.run(d)
+		#d = coref.run(d)
+		#d = date.run(d)
+	d = Salign.run(d)
+		#d = prop.run(d)
+		#d = SPOalign.run(d)
+	writer.run(d)
+	print "Document Title: %s \t Number of Annotated Entities %s \t Number of Annotated Triples %s" % (d.title, len(d.entities), len(d.triples))
+
  #   except Exception as e:
 
   #      print "error Processing document %s" % d.title
