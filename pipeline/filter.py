18074ba2c9b4419fd6efe005e3c883839420b541
hadyelsahar <hadyelsahar@gmail.com>
2017-08-03 20:40:01 +0100

    code cleaning of filters classes
    - local functions start by _
    - removing unused variables
    - adding doubles spaces before classes

--- pipeline/filter.py
+++ pipeline/filter.py
@@ -12,39 +12,40 @@ class SentenceLimiter:
         """
         boundaries = (document.sentences_boundaries[0][0], document.sentences_boundaries[:number_sentences+1][-1][1])
         document.text = document.text[boundaries[0]:boundaries[1]]
-        document.sentences_boundaries = self.limitSenteceBoundaries(document.sentences_boundaries, boundaries[1])
-        document.words_boundaries = self.limitWordBoundaries(document.words_boundaries, boundaries[1]) 
-        document.entities = self.limitEntities(document.entities, boundaries[1])
-        document.triples = self.limitTriples(document.triples, boundaries[1])
+        document.sentences_boundaries = self._limitSenteceBoundaries(document.sentences_boundaries, boundaries[1])
+        document.words_boundaries = self._limitWordBoundaries(document.words_boundaries, boundaries[1])
+        document.entities = self._limitEntities(document.entities, boundaries[1])
+        document.triples = self._limitTriples(document.triples, boundaries[1])
         return document
 
-    def limitSenteceBoundaries(self, sentences_boundaries, maxi):
+    def _limitSenteceBoundaries(self, sentences_boundaries, maxi):
         sentences_boundaries_new = []
         for sent in sentences_boundaries:
             if sent[1] <= maxi:
                 sentences_boundaries_new.append(sent) 
         return sentences_boundaries_new
 
-    def limitEntities(self, entities, maxi):
+    def _limitEntities(self, entities, maxi):
         entities_new = []
         for e in entities:
             if e.boundaries[1] <= maxi:
                 entities_new.append(e)
         return entities_new
 
-    def limitTriples(self, triples, maxi):
+    def _limitTriples(self, triples, maxi):
         triples_new = []
         for t in triples:
             if t.sentence_id == 0:
                 triples_new.append(t)
         return triples_new
 
-    def limitWordBoundaries(self, words_boundaries, maxi):
+    def _limitWordBoundaries(self, words_boundaries, maxi):
         words_boundaries_new = []
         for word in words_boundaries:
             if word[1] <= maxi:
                 words_boundaries_new.append(word) 
         return words_boundaries_new
+
 
 class MainEntityLimiter:
     """
@@ -55,14 +56,14 @@ class MainEntityLimiter:
             document = None
         return document
 
+
 class EntityTypeFilter:
     """
     Remove all documents that are of a certain type
     """
     def __init__(self, all_triples, entities):
         """
-        :param: input document containing the triples (two entities and
-        the property that bind them together)
+        :param: input TripleReaderTriples object
         :param: a list of entity that should be filtered
         """
         self.wikidata_triples = all_triples
@@ -71,8 +72,6 @@ class EntityTypeFilter:
     def run(self, document):
         # P31: instance of
         prop_id = 'http://www.wikidata.org/prop/direct/P31'
-        # Q4167410: Wikimedia disambiguation page
-        dis_id = 'http://www.wikidata.org/entity/Q4167410'
         if any([i for i in self.wikidata_triples.get(document.docid) if i[1] == prop_id and i[2] in self.entities]):
             document = None
         return document
