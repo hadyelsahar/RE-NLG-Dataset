52cb57ec57740d9cca3dae4e7c385c5cc9e37761
hadyelsahar <hadyelsahar@gmail.com>
2017-09-06 17:12:17 +0200

    new run file for JWS dataset

--- run.py
+++ run.py
@@ -1,11 +1,11 @@
-from pipeline.pipeline import *
-from pipeline.entitylinker import *
-from pipeline.triplealigner import *
+from utils.triplereadertriples import *
 from pipeline.datareader import DBpediaAbstractsDataReader
-from pipeline.writer import JsonWriter
+from pipeline.triplealigner import *
+from pipeline.entitylinker import *
+from pipeline.filter import *
+from pipeline.writer import *
 from pipeline.coreference import *
-from utils.triplereader import *
-
+from pipeline.placeholdertagger import *
 
 start_doc = 0   #start reading from document number #
 
@@ -13,33 +13,65 @@ start_doc = 0   #start reading from document number #
 reader = DBpediaAbstractsDataReader('./datasets/wikipedia-abstracts/csv/dbpedia-abstracts.csv', db_wd_mapping='./datasets/wikidata/dbpedia-wikidata-sameas-dict.csv', skip=start_doc)
 
 # Loading the WikidataSpotlightEntityLinker ... DBpedia Spotlight with mapping DBpedia URIs to Wikidata
-link = WikidataSpotlightEntityLinker('./datasets/wikidata/dbpedia-wikidata-sameas-dict.csv', support=10, confidence=0.4)
-
+link = WikidataSpotlightEntityLinkerWithCustomSupportAndFilter('./datasets/wikidata/dbpedia-wikidata-sameas-dict.csv')
 coref = SimpleCoreference()
-trip_read = TripleReader('./datasets/wikidata/wikidata-triples.csv')
-Salign = SimpleAligner(trip_read)
-prop = WikidataPropertyLinker('./datasets/wikidata/wikidata-properties.csv')
-date = DateLinker()
-SPOalign = SPOAligner(trip_read)
-NSalign = NoSubjectAlign(trip_read)
-writer = JsonWriter('./out', "re-nlg", startfile=start_doc)
+
+trip_read_trip = TripleReaderTriples('./datasets/wikidata/wikidata-triples.csv')
+
+# filters and limiters
+filter_entities = ['http://www.wikidata.org/entity/Q4167410', 'http://www.wikidata.org/entity/Q13406463']
+entity_filter = EntityTypeFilter(trip_read_trip, filter_entities)
+
+# limiting sentences
+sen_lim = SentenceLimiter()
+
+# checking if main entity of the document is found by an entity linker
+main_ent_lim = MainEntityLimiter()
+
+# placholder creation
+placeholder_tagger = TypePlaceholderTagger('./datasets/datatypes/wikidata_datatypes.csv')
+prop_tag = PropertyPlaceholderTagger()
+
+# adding triples from a knowledge base
+noalign = NoAligner(trip_read_trip)
+
+writer_triples = CustomeWriterTriples('./out-jws', "jws_trex", 10000)
+writer_entities = CustomeWriterEntities('./out-jws', "jws_trex", 10000)
+writer = JsonWriter('./out-jws', "jws_trex")
+
 
 for d in reader.read_documents():
 
     try:
+        print "Processing Document Title: %s ..." % d.title
+
+
+        if not entity_filter.run(d):
+            continue
+
+        d = sen_lim.run(d, 0)
+
         d = link.run(d)
 
-        d = date.run(d)
-        d = NSalign.run(d)
+        if not main_ent_lim.run(d):
+            continue
 
         d = coref.run(d)
-        d = Salign.run(d)
 
-        d = prop.run(d)
-        d = SPOalign.run(d)
+        d = coref.run(d)
+
+        d = noalign.run(d)
+
+        d = prop_tag.run(d)
+
+        writer_triples.run(d)
+        writer_entities.run(d)
         writer.run(d)
-        print "Document Title: %s \t Number of Annotated Entities %s \t Number of Annotated Triples %s" % (d.title, len(d.entities), len(d.triples))
+        print "Number of Annotated Entities %s \t Number of Annotated Triples %s \n -------" % (len(d.entities), len(d.triples))
 
     except Exception as e:
 
         print "error Processing document %s" % d.title
+
+
+
